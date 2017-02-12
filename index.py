932fac0773efb8b09dcd192daa9301eb0882e845
jadelu <wsluyu2011@163.com>
2017-02-12 22:45:01 +0800

    feat: complete status monitor

--- index.py
+++ index.py
@@ -3,34 +3,32 @@
 import util
 import time
 import json
+from urlparse import urlparse
+def geturl():
+    f = open('urlTest.txt','r')
+    urls = f.readlines()
+    f.close()
+    for i in range(0,len(urls)):
+        test_url = urls[i].strip().split(',')
+        hostname = urlparse(test_url[0]).netloc
+        path = urlparse(test_url[0]).path + '?' + urlparse(test_url[0]).query
+        res = util.fetch(hostname, path)
+        
+        if not(util.assertEqual(res.status, int(test_url[1]))):
+            # util.reportEmail('http status error:' + status)
+            print 'failed', test_url[0]
+        else:
+            print 'success', test_url[0]
 
-def test1():
-    res = util.fetch('jingyan.baidu.com', '/asyncreq?method=getRecommend&next=0&type=1')
-    status = res.status
-
-    if not(util.assertEqual(status, 200)):
-        util.reportEmail('http status error:' + status)
-        return
-
-    data = json.loads(res.read())
-
-    if util.assertEqual(data['errno'], 0):
-        util.reportFile('http content error')
-        return
-        # util.reportEmail('http content error')
 
 def run():
-    print 'start'
-    test1()
-    print 'end'
+    geturl()
+    
 
-count = 1   
+count = 0
 while True:
-    if count == 1:
-        run()
-        count = 0
-        time.sleep(1)
-    else:
-        count += 1
-        time.sleep(1)
-        print count
+    count += 1
+    print 'start:', count
+    run()
+    print 'end:', count
+    time.sleep(1)
